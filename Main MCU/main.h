8c3dd74efbf1bd5a80c14bff75ad8823729d51fd
umi <c.uaisyah02@gmail.com>
2026-08-18 11:30:13 +0800

    Add files via upload

---
+++ Main MCU/main.h
@@ -0,0 +1,93 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define MAIN_ADC_Pin GPIO_PIN_0
+#define MAIN_ADC_GPIO_Port GPIOA
+#define SUB_ADC_Pin GPIO_PIN_1
+#define SUB_ADC_GPIO_Port GPIOA
+#define DAC_MAIN_Pin GPIO_PIN_4
+#define DAC_MAIN_GPIO_Port GPIOA
+#define DAC_SUB_Pin GPIO_PIN_5
+#define DAC_SUB_GPIO_Port GPIOA
+#define UART_TX_Pin GPIO_PIN_9
+#define UART_TX_GPIO_Port GPIOA
+#define UART_RX_Pin GPIO_PIN_10
+#define UART_RX_GPIO_Port GPIOA
+#define HEARTBEAT_Pin GPIO_PIN_12
+#define HEARTBEAT_GPIO_Port GPIOA
+#define MCU_SWDIO_Pin GPIO_PIN_13
+#define MCU_SWDIO_GPIO_Port GPIOA
+#define MCU_SWDCLK_Pin GPIO_PIN_14
+#define MCU_SWDCLK_GPIO_Port GPIOA
+#define MCU_GATE_ENABLE_Pin GPIO_PIN_15
+#define MCU_GATE_ENABLE_GPIO_Port GPIOA
+#define CAN_RX_Pin GPIO_PIN_8
+#define CAN_RX_GPIO_Port GPIOB
+#define CAN_TX_Pin GPIO_PIN_9
+#define CAN_TX_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
